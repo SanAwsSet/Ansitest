0b0f6f5e1d81afbb66cae89e2ce09923216b500e
Santosh Sethia <santoshsethiaaws@gmail.com>
2023-03-06 16:18:07 +0530

    Nornir

--- Nornir_Test.py
+++ Nornir_Test.py
@@ -1,6 +1,9 @@
 from nornir import InitNornir
 from nornir_utils.plugins.functions import print_result
 from nornir_napalm.plugins.tasks import napalm_get
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
 
 nr = InitNornir(
     config_file="config.yaml", dry_run=True
